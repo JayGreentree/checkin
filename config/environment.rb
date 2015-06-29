a2e4fae77e31b3c0a52fe2c3f367f9ca628a361d
Meg Richards <mouse@cmu.edu>
2015-06-29 03:26:53 -0400

    Add require 'yaml' to env, because activeldap assumes it already is. N.B.: Did not want to monkeypatch gem & error was happening before initializers load

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,10 @@
 # Load the Rails application.
 require File.expand_path('../application', __FILE__)
 
+# ActiveLDAP assumes yaml is already loaded (probs by sprockets)
+# Bad assumptions make this gross, but less gross than monkey patching
+# the gem.
+require 'yaml'
+
 # Initialize the Rails application.
 Rails.application.initialize!
